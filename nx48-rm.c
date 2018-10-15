c19e736f672cf07a0f89f6a37af084b3875d3a3e
spartrekus <spartrekus@gmail.com>
2018-10-15 19:56:01 +0200

    Add files via upload

---
+++ nx48-rm.c
@@ -0,0 +1,18 @@
+
+#include <stdio.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+// http://www.linuxfocus.org/common/src/article319/x48-gxrom-r.tar.gz
+
+
+
+int main()
+{
+    system( " cd ; wget http://www.linuxfocus.org/common/src/article319/x48-gxrom-r.tar.gz " ) ; 
+    system( " cd ; tar xvpfz  x48-gxrom-r.tar.gz " ) ; 
+    return 0;
+}
+
