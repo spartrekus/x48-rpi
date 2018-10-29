6e982f5af29088e5935ac714d18837db67e59b6a
spartrekus <spartrekus@gmail.com>
2018-10-29 16:33:36 +0100

    Add files via upload

---
+++ x48.sh
@@ -0,0 +1,1 @@
+xlsfonts ; x48 -connFont -misc-fixed-medium-r-semicondensed--0-0-75-75-c-0-iso8859-1     -smallFont -misc-fixed-medium-r-semicondensed--0-0-75-75-c-0-iso8859-1   -mediumFont -misc-fixed-medium-r-semicondensed--13-100-100-100-c-60-iso8859-1  -largeFont -misc-fixed-medium-r-semicondensed--13-120-75-75-c-60-iso8859-1    
