f177774c27fb28e08ad7d8da4169e57a1d5674b8
spartrekus <spartrekus@gmail.com>
2018-11-03 09:49:36 +0100

    Add files via upload

--- nc48.c
+++ nc48.c
@@ -208,7 +208,8 @@ int main()
 
          case 'x':
             gfxcls();
-            nruncmd(  " ./x48 ");
+            //nruncmd(  " ./x48 ");
+            nruncmd( " xlsfonts ; ./x48 -connFont -misc-fixed-medium-r-semicondensed--0-0-75-75-c-0-iso8859-1     -smallFont -misc-fixed-medium-r-semicondensed--0-0-75-75-c-0-iso8859-1   -mediumFont -misc-fixed-medium-r-semicondensed--13-100-100-100-c-60-iso8859-1  -largeFont -misc-fixed-medium-r-semicondensed--13-120-75-75-c-60-iso8859-1     " );
             mvprintw( rows-2, 5, "x48 completed" );
             getch(); 
             break;
